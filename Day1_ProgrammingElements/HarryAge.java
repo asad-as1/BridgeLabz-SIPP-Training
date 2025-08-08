5a0a2ca5658d2a199eaae3af6cab96d9c52ea61b
Mohd Asad Ansari <asad.as9458@gmail.com>
2025-08-08 23:54:28 +0530

    Remove folder_name from tracking

--- Day1_ProgrammingElements/HarryAge.java
+++
@@ -1,8 +0,0 @@
-public class HarryAge {
-    public static void main(String[] args) {
-        int dob = 2000;
-        int currentYear = 2024;
-        int age = currentYear - dob;
-        System.out.println("Age: " + age);
-    }
-}
